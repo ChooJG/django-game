ddcd548e8ca9de6cb4dfcf9ff39f55bd51639c49
ChooJG <cnwprms@naver.com>
2024-09-02 01:44:15 +0900

    공룡게임 캐릭터 변경

--- dino/views.py
+++ dino/views.py
@@ -5,11 +5,17 @@ from django.template import loader
 from datetime import datetime
 
 
-#메인화면으로 이동
+#dino 게임 메인화면으로 이동
 def index(request):
     now = datetime.now()
 
     context = {
         'current_time' : now
     }
-    return render(request, 'main/index.html', context)
+    return render(request, 'dino/index.html', context)
+
+
+#dino 게임화면으로 이동
+def game(request):
+
+    return render(request, 'dino/game.html')
--- firstdjango/urls.py
+++ firstdjango/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('main/', include('main.urls')),
+    path('dino/', include('dino.urls')),
     path('admin/', admin.site.urls),
 ]
