140f4d69a132eba1cc55f922c131074efff1a8bd
ChooJG <cnwprms@naver.com>
2024-09-02 22:17:18 +0900

    메인페이지 및 네비게이션 바 추가

--- dino/urls.py
+++ dino/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-   path('', views.index, name='index'),
-   path('game/', views.game, name="game")
+   path('', views.index, name='dino-main'),
+   path('game/', views.game, name="dino-game")
 ]
